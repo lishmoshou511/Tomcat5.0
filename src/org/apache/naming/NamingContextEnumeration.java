72399760fae5611a4bad91a194e92de7157f4d29
lishuang <lish516@126.com>
2014-08-21 13:17:51 +0800

    Remove all the enum keyworks.

--- src/org/apache/naming/NamingContextEnumeration.java
+++ src/org/apache/naming/NamingContextEnumeration.java
@@ -85,12 +85,12 @@ public class NamingContextEnumeration
 
 
 	public NamingContextEnumeration(Vector entries) {
-		enum = entries.elements();
+		enum1 = entries.elements();
 	}
 
 
-	public NamingContextEnumeration(Enumeration enum) {
-		this.enum = enum;
+	public NamingContextEnumeration(Enumeration enum1) {
+		this.enum1 = enum1;
 	}
 
 
@@ -100,7 +100,7 @@ public class NamingContextEnumeration
 	/**
 	 * Underlying enumeration.
 	 */
-	protected Enumeration enum;
+	protected Enumeration enum1;
 
 
 	// --------------------------------------------------------- Public Methods
@@ -120,7 +120,7 @@ public class NamingContextEnumeration
 	 */
 	public boolean hasMore()
 			throws NamingException {
-		return enum.hasMoreElements();
+		return enum1.hasMoreElements();
 	}
 
 
@@ -133,12 +133,12 @@ public class NamingContextEnumeration
 
 
 	public boolean hasMoreElements() {
-		return enum.hasMoreElements();
+		return enum1.hasMoreElements();
 	}
 
 
 	public Object nextElement() {
-		NamingEntry entry = (NamingEntry) enum.nextElement();
+		NamingEntry entry = (NamingEntry) enum1.nextElement();
 		return new NameClassPair(entry.name, entry.value.getClass().getName());
 	}
 
